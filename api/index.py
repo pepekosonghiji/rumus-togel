33e4c21deea8f9ebf499d59bb0639e0eb4c21cc2
pepekosonghiji <pepekosonghiji@gmail.com>
2026-02-18 01:41:23 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -64,7 +64,24 @@ def get_comprehensive_logic(all_res, m_name):
         "macau": f"{SHIO_MAP.get(int(d0[2:]) % 12)} - {SHIO_MAP.get((int(d0[2:]) % 12 + 6) % 12)}",
         "twin": f"{d0[3]}{d0[3]}, {bbfs[0]}{bbfs[0]}"
     }
-    elif m_name == "TORONTOMID": line = [MB.get(d0[1])+TY.get(d0[3]), "76", "19", "66", "53"]
+    elif m_name == "TORONTOMID": 
+        d0 = all_res[0]
+        bbfs = get_refined_bbfs(all_res, limit=35)
+        if '1' not in bbfs: bbfs.append('1')
+        l1 = TY.get(d0[2], '0') + MB.get(d0[3], '0')
+        l2 = ML.get(d0[0], '0') + ID.get(d0[1], '0')
+        l3 = "02" if "0" in bbfs else "39"
+        l4 = d0[3] + d0[2]
+        core_lines = list(dict.fromkeys([l1, l2, l3, l4, "42", "72"]))
+        return {
+        "core": ", ".join(core_lines[:5]),
+        "bbfs": " ".join(sorted(bbfs[:6])), # Batasi tetap 6 digit
+        "as_kop": TY.get(d0[0], '0') + TY.get(d0[1], '0'), # Antisipasi twin lagi
+        "kop_kep": ID.get(d0[1], '0') + MB.get(d0[2], '0'),
+        "shio": SHIO_MAP.get(int(d0[2:]) % 12, "N/A"),
+        "macau": f"{SHIO_MAP.get(int(d0[2:]) % 12)} - {SHIO_MAP.get((int(d0[2:]) % 12 + 6) % 12)}",
+        "twin": f"{d0[0]}{d0[0]}, 11" # Fokus twin 22 atau 11
+    }
     elif m_name == "HONGKONG LOTTO":
         line.extend([ID.get(d0[0])+ML.get(d0[3]), TY.get(d0[1])+d0[2], MB.get(d0[3])+d0[0]])
     elif m_name == "CAMBODIA":
