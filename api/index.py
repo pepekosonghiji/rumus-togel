2f6f57ddbc9f7aaa8af0c3be9860c67a13ea602c
pepekosonghiji <pepekosonghiji@gmail.com>
2026-02-24 22:02:26 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -905,7 +905,7 @@ def fetch_results(market_code):
                 return res[:40]
             
             # Jalur Umum (Tetap sama, tapi tambahkan proteksi list)
-            url = f"https://8eblsoypit.salamrupiah.com/history/result-mobile/{market_code}-pool-1"
+            url = f"https://nfx1avfcy8.salamtarget.com/history/result-mobile/{market_code}-pool-1"
             r = client.get(url, headers=headers)
             soup = BeautifulSoup(r.text, 'html.parser')
             table = soup.find('table', class_='table-history')
