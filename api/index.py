04ea81179972c1a444f18faf0ef963a55e305188
pepekosonghiji <pepekosonghiji@gmail.com>
2026-02-25 23:40:39 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -897,41 +897,41 @@ def generate_titanium_lines_v14(bbfs_list, last_p1, market_name, scores, all_res
 
         # --- [ V17.0 HK-MONSTER: SHADOW & BRIDGE SHIFTING ] ---
         elif market_name == 'HONGKONG LOTTO':
-        try:
-            # Result: 6314
-            as_l, kop_l, kep_l, ek_l = last_p1[0], last_p1[1], last_p1[2], last_p1[3]
+            try:
+                # Result: 6314
+                as_l, kop_l, kep_l, ek_l = last_p1[0], last_p1[1], last_p1[2], last_p1[3]
 
-            if i == 0:
+                if i == 0:
                 # POLA REVERSAL (Ekor lama jadi As baru, Kop lama jadi Kop baru)
-                asn = ek_l # 4
-                kop = ID.get(kop_l, '8') # 3 -> 8 (Hasil: 48xx)
-            elif i == 1:
+                    asn = ek_l # 4
+                    kop = ID.get(kop_l, '8') # 3 -> 8 (Hasil: 48xx)
+                elif i == 1:
                 # POLA SHADOW SHIFT (Indeks Kepala jadi As)
-                asn = ID.get(kep_l, '6') # 1 -> 6
-                kop = ML.get(as_l, '9')  # 6 -> 9 (Hasil: 69xx)
-            elif i == 2:
+                    asn = ID.get(kep_l, '6') # 1 -> 6
+                    kop = ML.get(as_l, '9')  # 6 -> 9 (Hasil: 69xx)
+                elif i == 2:
                 # POLA NEUTRALIZER (05 Anchor)
-                asn, kop = '0', '5' 
-            else:
-                asn = best_as[i % len(best_as)]
-                kop = best_kop[(i + 1) % len(best_kop)]
+                    asn, kop = '0', '5' 
+                else:
+                    asn = best_as[i % len(best_as)]
+                    kop = best_kop[(i + 1) % len(best_kop)]
 
             # --- 2D SNIPER FILTER V18.1 ---
             # 1. BIJI SIKLUS (Fokus Biji 2, 5, 8 - Siklus Lompat +3)
-            if biji_f in [2, 5, 8]: score += 120
+                if biji_f in [2, 5, 8]: score += 120
             
             # 2. POSITION LOCK (Ganjil-Genap Shifting)
             # Result 6314 (Genap-Ganjil-Ganjil-Genap) -> Target Ganjil di Ekor!
-            if int(t) % 2 != 0: score += 60
+                if int(t) % 2 != 0: score += 60
             
             # 3. ANTI-SAME (Buang angka 6, 3, 1, 4 di posisi 2D belakang)
-            if h in p1_d or t in p1_d:
-                score -= 40 
+                if h in p1_d or t in p1_d:
+                    score -= 40 
 
-            line3, line4 = f"{kop}{l2}", f"{asn}{kop}{l2}"
-            if line3 not in top3: top3.append(line3)
-            if line4 not in top4: top4.append(line4)
-        except: pass
+                line3, line4 = f"{kop}{l2}", f"{asn}{kop}{l2}"
+                if line3 not in top3: top3.append(line3)
+                if line4 not in top4: top4.append(line4)
+            except: pass
 
         # --- [ V17.0 HKP MONSTER CONSTRUCTION ] ---
         elif market_name == 'HONGKONG POOLS':
