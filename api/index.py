9e2589e88ae271972a7fe9244fed09d44d49b3c8
pepekosonghiji <pepekosonghiji@gmail.com>
2026-02-24 21:21:59 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1021,4 +1021,4 @@ def index():
     return render_template('index.html', markets=markets, analysis=analysis, selected=selected)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
